fb82700b515cf52ba44468f2f2ea2a140ded6362
Spenser Black <spenserblack01@gmail.com>
2020-01-11 19:34:54 -0500

    Add column struct

--- src/main.rs
+++ src/main.rs
@@ -20,3 +20,54 @@ fn main() {
 
     let conn = Connection::open(db).unwrap();
 }
+
+mod table_data {
+    use std::str::FromStr;
+    
+    pub struct Column<'a> {
+        name: &'a str,
+        not_null: bool,
+    }
+    
+    impl<'a> FromStr for Column<'a> {
+        type Err = String; // TODO Use better error type
+        
+        // TODO Use Regex for better parsing
+        fn from_str(s: &str) -> Result<Self, Self::Err> {
+            let mut not_null = false;
+            let name: String = s.chars().enumerate().filter_map(|(i, c) {
+                if i == 0 && c == '!' {
+                    not_null = true;
+                    None
+                } else {
+                    Some(c)
+                }
+            }|).collect();
+            let name = name.as_str();
+            
+            Column {
+                not_null,
+                name,
+            }
+        }
+    }
+    
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+        
+        #[test]
+        fn plain_column() {
+            let column = "Title".parse().unwrap();
+            assert_eq!("Title", column.name);
+            assert_eq!(false, column.not_null);
+        }
+        
+        #[test]
+        fn important_column() {
+            let column = "!Title".parse().unwrap();
+            assert_eq!("Title", column.name);
+            assert_eq!(true, column.not_null);
+        }
+    }
+}
