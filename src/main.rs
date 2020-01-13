d8f6ef17f66ff965865462265048bef3cc6d0714
Spenser Black <spenserblack01@gmail.com>
2020-01-13 08:10:37 -0500

    Put CLI in separate module

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,13 @@
-use clap::{App, Arg, crate_description, crate_name, crate_version};
 use rusqlite::{Connection, NO_PARAMS};
 
 const DEFAULT_DB_NAME: &str = "favlist.sqlite";
 
 fn main() {
-
-    let matches = App::new(crate_name!())
-        .version(crate_version!())
-        // .about(crate_description!())
-        .arg(Arg::with_name("database")
-            .long("db")
-            .value_name("FILE")
-            .help("The file to save/load your list(s)")
-            .takes_value(true)
-            .default_value(DEFAULT_DB_NAME))
-        .get_matches();
+    let matches = cli::app().get_matches();
 
     let db = matches.value_of("database").unwrap();
 
     let conn = Connection::open(db).unwrap();
 }
+
+mod cli;
