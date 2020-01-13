493b5d0803e1c3b18ed6435e8bc8f18835a00fe7
Spenser Black <spenserblack01@gmail.com>
2020-01-13 09:45:54 -0500

    Use Regex to parse to column

--- src/table_data.rs
+++ src/table_data.rs
@@ -1,8 +1,20 @@
+use regex::Regex;
 use std::str::FromStr;
 
 pub struct Column {
     name: String,
     not_null: bool,
+    data_type: DataType,
+}
+
+#[derive(Debug)]
+enum DataType {
+    Integer,
+    Text,
+    /// No datatype
+    Blob,
+    Real,
+    Numeric,
 }
 
 impl FromStr for Column {
@@ -10,19 +22,26 @@ impl FromStr for Column {
 
     // TODO Use Regex for better parsing
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let mut not_null = false;
-        let name: String = s.chars().enumerate().filter_map(|(i, c)| {
-            if i == 0 && c == '!' {
-                not_null = true;
-                None
-            } else {
-                Some(c)
+        let regex = Regex::new(r"(?P<important>^!)?(?P<name>\w+)@?(?P<type>\w+)?").unwrap();
+        let caps = regex.captures(s).unwrap();
+        let not_null = caps.name("important").is_some();
+        let name = caps.name("name").unwrap().as_str().into();
+        let data_type = if let Some(cap) = caps.name("type") {
+            match cap.as_str() {
+                "int" => DataType::Integer,
+                "text" => DataType::Text,
+                "real" => DataType::Real,
+                "num" => DataType::Numeric,
+                s => unimplemented!("{}", s),
             }
-        }).collect();
+        } else {
+            DataType::Blob
+        };
 
         let column = Column {
             not_null,
             name,
+            data_type,
         };
 
         Ok(column)
@@ -31,6 +50,7 @@ impl FromStr for Column {
 
 #[cfg(test)]
 mod tests {
+    use assert_matches::assert_matches;
     use super::*;
 
     #[test]
@@ -38,6 +58,17 @@ mod tests {
         let column: Column = "Title".parse().unwrap();
         assert_eq!("Title", column.name);
         assert_eq!(false, column.not_null);
+        let data_type = column.data_type;
+        assert_matches!(DataType::Blob, data_type);
+    }
+
+    #[test]
+    fn int_column() {
+        let column: Column = "Year@int".parse().unwrap();
+        assert_eq!("Year", column.name);
+        assert_eq!(false, column.not_null);
+        let data_type = column.data_type;
+        assert_matches!(DataType::Integer, data_type);
     }
 
     #[test]
@@ -45,5 +76,16 @@ mod tests {
         let column: Column = "!Title".parse().unwrap();
         assert_eq!("Title", column.name);
         assert_eq!(true, column.not_null);
+        let data_type = column.data_type;
+        assert_matches!(DataType::Blob, data_type);
+    }
+
+    #[test]
+    fn important_int_column() {
+        let column: Column = "!Year@int".parse().unwrap();
+        assert_eq!("Year", column.name);
+        assert_eq!(true, column.not_null);
+        let data_type = column.data_type;
+        assert_matches!(DataType::Integer, data_type);
     }
 }
